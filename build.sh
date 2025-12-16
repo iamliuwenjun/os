b9d4f8d177111a1447a876773c68d911aa99f202
lwj <1642266876@qq.com>
2025-12-16 18:12:59 +0800

    添加脚本：build.sh（编译QEMU）和run.sh（启动quard-star板子）

---
+++ build.sh
@@ -0,0 +1,10 @@
+# 获取当前脚本文件所在的目录
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+
+cd qemu-8.0.2
+if [ ! -d "$SHELL_FOLDER/output/qemu" ]; then  
+./configure --prefix=$SHELL_FOLDER/output/qemu  --target-list=riscv64-softmmu --enable-gtk  --enable-virtfs --disable-gio
+fi  
+make -j16
+sudo make install
+cd ..
---
+++ run.sh
@@ -0,0 +1,5 @@
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+$SHELL_FOLDER/output/qemu/bin/qemu-system-riscv64 \
+-M quard-star \
+-m 1G \
+-smp 8 \
