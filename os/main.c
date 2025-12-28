f2f2cc6dbababb7e9af0d1133ec1ef3c0353e3bb
lwj <1642266876@qq.com>
2025-12-28 23:44:31 +0800

    封装printf函数

--- os/main.c
+++ os/main.c
@@ -1,11 +1,8 @@
-extern sbi_console_putchar(int ch);
+#include "os.h"
+
 
 void os_main()
 {
-    sbi_console_putchar('h');
-    sbi_console_putchar('e');
-    sbi_console_putchar('l');
-    sbi_console_putchar('l');
-    sbi_console_putchar('o');
-    sbi_console_putchar('!');
+   int a = 666;
+   printf("hello lwj os!%d",a);
 }
