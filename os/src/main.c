9cb71ae658f49193934ea146ccfe17e25385d523
lwj <1642266876@qq.com>
2026-01-29 20:34:51 +0800

    修改syscall函数以保证sys_gettime函数获取时间的正确；在type.h添加类型

--- os/src/main.c
+++ os/src/main.c
@@ -4,16 +4,16 @@ extern void frame_allocator_test();
 void os_main()
 {
    printk("hello timer os!\n");
-   frame_allocator_test();
-   while (1)
-   {
-      /* code */
-   }
-   // trap_init();
+   // frame_allocator_test();
+   // while (1)
+   // {
+   //    /* code */
+   // }
+   trap_init();
 
-   // task_init();
+   task_init();
 
-   // timer_init();
+   timer_init();
    
-   // run_first_task();
+   run_first_task();
 }
